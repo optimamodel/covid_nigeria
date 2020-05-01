82d0059ed21e10c00e5520419d14dfd37b884397
cliffckerr <questionably@gmail.com>
2020-04-30 23:52:31 -0700

    very large numbers of infections

--- calibration_nigeria_may01.py
+++ calibration_nigeria_may01.py
@@ -1,14 +1,24 @@
 import covasim as cv
 
 pars = dict(
-    pop_size = 20e3,
+    pop_size = 200e3,
     pop_scale = 5,
-    start_day = '2020-03-01',
+    start_day = '2020-02-15',
     pop_infected = 5,
     interventions = cv.test_prob(symp_prob=0.01, asymp_prob=0),
-    n_days = 60,
+    n_days = 75,
+    rand_seed = 2,
+    beta = 0.012,
+    location = 'nigeria',
+    pop_type = 'hybrid',
     )
+
+to_plot = cv.get_sim_plots()
+to_plot['Diagnoses'] = ['cum_diagnoses']
+to_plot['Deaths'] =  ['cum_deaths']
+to_plot.remove('Health outcomes')
+
 
 sim = cv.Sim(pars=pars, datafile='nigeria_data.csv')
 sim.run()
-sim.plot()
+sim.plot(to_plot=to_plot)
