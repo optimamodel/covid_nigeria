a8e26b139c70a7bc83d7790600c53a8f3f3635ce
cliffckerr <questionably@gmail.com>
2020-05-01 00:10:49 -0700

    getting reasonable results now it seems?

--- calibration_nigeria_may01.py
+++ calibration_nigeria_may01.py
@@ -1,24 +1,26 @@
 import covasim as cv
 
 pars = dict(
-    pop_size = 200e3,
+    pop_size = 50e3,
     pop_scale = 1,
     start_day = '2020-02-15',
-    pop_infected = 5,
-    interventions = cv.test_prob(symp_prob=0.01, asymp_prob=0),
+    # pop_infected = 5,
+    # interventions = cv.test_prob(symp_prob=0.01, asymp_prob=0),
     n_days = 75,
-    rand_seed = 2,
-    beta = 0.012,
-    location = 'nigeria',
-    pop_type = 'hybrid',
+    # rand_seed = 2,
+    # beta = 0.012,
+    # location = 'nigeria',
+    # pop_type = 'hybrid',
     )
 
 # Works
-pars = dict(pop_size=200e3,
-             n_days=60,
-             rand_seed=1,
-             pop_type = 'hybrid',
-             )
+# pars = dict(pop_size=200e3,
+#             start_day = '2020-02-15',
+#             pop_scale = 1,
+#               n_days=60,
+#               rand_seed=1,
+#               pop_type = 'hybrid',
+#               )
 
 to_plot = cv.get_sim_plots()
 to_plot['Diagnoses'] = ['cum_diagnoses']
