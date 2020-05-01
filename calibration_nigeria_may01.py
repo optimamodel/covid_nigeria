193c6bbfd219f56711de28572a68860c5ad06a97
cliffckerr <questionably@gmail.com>
2020-05-01 01:52:28 -0700

    decent calibration

--- calibration_nigeria_may01.py
+++ calibration_nigeria_may01.py
@@ -1,29 +1,26 @@
 import covasim as cv
 
+# Calibration parameters
+symp_prob = 0.007
+beta_change = 0.5
+beta = 0.012
+
+# Other parameters
 pars = dict(
     pop_size = 300e3,
     pop_scale = 1.0,
     start_day = '2020-02-25',
-    pop_infected = 25,
+    pop_infected = 200,
     interventions = [
-        cv.test_prob(symp_prob=0.01, asymp_prob=0),
-        cv.change_beta(days='2020-03-29', changes=0.6, layers=['s','w','c']),
+        cv.test_prob(symp_prob=symp_prob, asymp_prob=0),
+        cv.change_beta(days='2020-03-29', changes=beta_change, layers=['s','w','c']),
         ],
     n_days = 65,
-    rand_seed = 2,
-    # beta = 0.012,
+    rand_seed = 298347,
+    beta = beta,
     location = 'nigeria',
     pop_type = 'hybrid',
     )
-
-# Simple
-# pars = dict(pop_size=200e3,
-#             start_day = '2020-02-15',
-#             pop_scale = 1,
-#               n_days=60,
-#               rand_seed=1,
-#               pop_type = 'hybrid',
-#               )
 
 to_plot = cv.get_sim_plots()
 to_plot['Diagnoses'] = ['cum_diagnoses']
