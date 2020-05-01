e7e2d0564937d8a169a753b2714222712e088c86
cliffckerr <questionably@gmail.com>
2020-04-30 23:43:46 -0700

    good fit to deaths

--- calibration_nigeria_may01.py
+++ calibration_nigeria_may01.py
@@ -1,5 +1,13 @@
 import covasim as cv
 
-sim = cv.Sim(datafile='nigeria_data.csv')
+pars = dict(
+    pop_size = 20e3,
+    start_day = '2020-02-15',
+    pop_infected = 10,
+    interventions = cv.test_prob(symp_prob=0.01, asymp_prob=0),
+    n_days = 80,
+    )
+
+sim = cv.Sim(pars=pars, datafile='nigeria_data.csv')
 sim.run()
 sim.plot()
