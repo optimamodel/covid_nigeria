27973299c57a35226dd1571479263049f5561623
cliffckerr <questionably@gmail.com>
2020-04-30 23:44:09 -0700

    good fit to diagnoses but not deaths

--- calibration_nigeria_may01.py
+++ calibration_nigeria_may01.py
@@ -2,10 +2,11 @@ import covasim as cv
 
 pars = dict(
     pop_size = 20e3,
-    start_day = '2020-02-15',
-    pop_infected = 10,
+    pop_scale = 10,
+    start_day = '2020-03-01',
+    pop_infected = 5,
     interventions = cv.test_prob(symp_prob=0.01, asymp_prob=0),
-    n_days = 80,
+    n_days = 60,
     )
 
 sim = cv.Sim(pars=pars, datafile='nigeria_data.csv')
