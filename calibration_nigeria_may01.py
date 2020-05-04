1e1b5d9402174973e52bfe3cd9955ed667c1a726
Robyn Stuart <robyna.s@gmail.com>
2020-05-05 09:49:56 +1000

    update

--- calibration_nigeria_may01.py
+++ calibration_nigeria_may01.py
@@ -33,12 +33,18 @@ pars = dict(
         cv.test_prob(symp_prob=symp_prob, asymp_prob=0, start_day=0, end_day='2020-05-04', do_plot=False),
         cv.change_beta(days=['2020-03-29','2020-05-04'], changes=[beta_change, 0.95], layers=['s','w','c'], do_plot=beta_change<1.0),
         ],
-    n_days = 92,
+    n_days = 65, #92,
     rand_seed = 111,
     beta = beta,
     location = 'nigeria',
     pop_type = 'hybrid',
-    )
+    n_beds =  1e-6,
+    OR_no_treat = 100.0,
+    rel_symp_prob = 1.,
+    rel_severe_prob = 4.5,
+    rel_crit_prob = 5.,
+
+)
 
 # Create sim and run
 sim = cv.Sim(pars=pars, datafile='nigeria_data.csv')
@@ -55,5 +61,5 @@ to_plot['Diagnoses'] = ['cum_diagnoses']
 to_plot['Deaths'] =  ['cum_deaths']
 to_plot.remove('Health outcomes')
 sim.plot(to_plot=to_plot, do_save=True, do_show=False, use_grid=False)
-sim.save('nigeria.sim')
+sim.save('nigeria_deaths.sim')
 
